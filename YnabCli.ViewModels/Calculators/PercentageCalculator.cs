17dd66cbaca800af73dee1098a593ac060862d7d
Joshua Crowe <joshuaedwardcrowe@icloud.com>
2025-02-27 20:48:32 +0000

    Fixed PercentageCalculator Calculation

--- YnabCli.ViewModels/Calculators/PercentageCalculator.cs
+++ YnabCli.ViewModels/Calculators/PercentageCalculator.cs
@@ -2,20 +2,20 @@ namespace YnabCli.ViewModels.Calculators;
 
 public static class PercentageCalculator
 {
-    public static int CalculateChange(decimal biggerValue, decimal smallerValue)
+    public static int CalculateChange(decimal originalValue, decimal newValue)
     {
-        
-        // 60 - 40 = 20
-        decimal difference = biggerValue - smallerValue;
-        
         // Avoid dividing by zero.
-        if (biggerValue == 0)
+        if (originalValue == 0)
         {
             return 0;
         }
+        
+        // 60 - 40 = 20
+        var difference = newValue - originalValue;
+        
 
         // (20 / 60) * 100 = 33.33
-        var percentage = (difference / biggerValue) * 100;
+        var percentage = difference / originalValue * 100;
         
         // (int)33.33 = 33
         return (int)percentage;
