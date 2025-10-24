f09f06ac33db84f80f0e1aacb3d5baf8868c1403
Joshua Crowe <joshuaedwardcrowe@icloud.com>
2025-10-24 22:19:28 +0100

    Deleted legacy stuff

--- YnabCli.Abstractions/YnabCliException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace YnabCli.Abstractions;
-
-// TODO: Convert to CliException & Cli.Abstractions.
-public class YnabCliException : Exception
-{
-    public YnabCliException(string message) : base(message)
-    {
-    }
-}
