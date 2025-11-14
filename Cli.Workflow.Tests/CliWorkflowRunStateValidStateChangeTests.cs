243b094adeb6f1fc6c8b501dfae18dd12aa35285
Joshua Crowe <joshuaedwardcrowe@icloud.com>
2025-11-14 18:39:48 +0000

    #120: Simplified CliWorkflowRunStateTests

--- Cli.Workflow.Tests/CliWorkflowRunStateValidStateChangeTests.cs
+++ Cli.Workflow.Tests/CliWorkflowRunStateValidStateChangeTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace Cli.Workflow.Tests;
 
 [TestFixture]
-public class CliWorkflowRunStateTests
+public class CliWorkflowRunStateValidStateChangeTests
 {
     public static IEnumerable<TestCaseData> ValidStateChanges()
     {
@@ -44,59 +44,6 @@ public class CliWorkflowRunStateTests
         ).SetName("State can be changed from Running to Finished");
     }
     
-    public static IEnumerable<TestCaseData> InvalidStateChanges()
-    {
-        yield return new TestCaseData(
-            Enumerable.Empty<ClIWorkflowRunStateType>(),
-            ClIWorkflowRunStateType.NotInitialized
-        ).SetName("State cannot be NotYetInitialized when already NotInitialized");
-        
-        yield return new TestCaseData(
-            new [] { ClIWorkflowRunStateType.Created },
-            ClIWorkflowRunStateType.Created
-        ).SetName("State cannot be Created when already Created");
-        
-        yield return new TestCaseData(
-            Enumerable.Empty<ClIWorkflowRunStateType>(),
-            ClIWorkflowRunStateType.Running
-        ).SetName("State cannot be Running when not NotInitialized");
-        
-        yield return new TestCaseData(
-            new [] { ClIWorkflowRunStateType.Created, ClIWorkflowRunStateType.Running },
-            ClIWorkflowRunStateType.Running
-        ).SetName("State cannot be Running when already Running");
-        
-        yield return new TestCaseData(
-            Enumerable.Empty<ClIWorkflowRunStateType>(),
-            ClIWorkflowRunStateType.InvalidAsk
-        ).SetName("State cannot be InvalidAsk when not NotInitialized");
-        
-        yield return new TestCaseData(
-            new [] { ClIWorkflowRunStateType.Created, ClIWorkflowRunStateType.Running, ClIWorkflowRunStateType.InvalidAsk},
-            ClIWorkflowRunStateType.InvalidAsk
-        ).SetName("State cannot be InvalidAsk when already InvalidAsk");
-        
-        yield return new TestCaseData(
-            Enumerable.Empty<ClIWorkflowRunStateType>(),
-            ClIWorkflowRunStateType.Exceptional
-        ).SetName("State cannot be Exceptional when not NotInitialized");
-        
-        yield return new TestCaseData(
-            new [] { ClIWorkflowRunStateType.Created, ClIWorkflowRunStateType.Running, ClIWorkflowRunStateType.Exceptional },
-            ClIWorkflowRunStateType.Exceptional
-        ).SetName("State cannot be InvalidAsk when already Exceptional");
-        
-        yield return new TestCaseData(
-            Enumerable.Empty<ClIWorkflowRunStateType>(),
-            ClIWorkflowRunStateType.Finished
-        ).SetName("State cannot be Finished when not NotInitialized");
-        
-        yield return new TestCaseData(
-            new [] { ClIWorkflowRunStateType.Created, ClIWorkflowRunStateType.Running, ClIWorkflowRunStateType.Finished },
-            ClIWorkflowRunStateType.Finished
-        ).SetName("State cannot be Finished when already Finished");
-    }
-    
     [TestCaseSource(nameof(ValidStateChanges))]
     public void GivenStateIs_WhenChangeTo_CanBeChanged(IEnumerable<ClIWorkflowRunStateType> priorStates, ClIWorkflowRunStateType stateToChangeTo)
     {
@@ -133,19 +80,5 @@ public class CliWorkflowRunStateTests
         Assert.That(stateChange, Is.Not.Null);
         Assert.That(stateChange!.StartedAt, Is.EqualTo(priorStateChange));
         Assert.That(stateChange.MovedTo, Is.EqualTo(stateToChangeTo));
-    }
-
-    [TestCaseSource(nameof(InvalidStateChanges))]
-    public void GivenStateIs_WhenChangedTo_CannotBeChanged(IEnumerable<ClIWorkflowRunStateType> priorStates, ClIWorkflowRunStateType stateToChangeTo)
-    {
-        var state = new CliWorkflowRunState();
-
-        foreach (var priorState in priorStates)
-        {
-            state.ChangeTo(priorState);
-        }
-        
-        // Act & Assert
-        Assert.Throws<ImpossibleStateChangeException>(() => state.ChangeTo(stateToChangeTo));
     }
 }
