94c63f1d31e9cbc589e31783f954cd90b99be0dc
Joshua Crowe <joshuaedwardcrowe@icloud.com>
2026-01-24 18:29:39 +0000

    Updated DI Package in Others

--- Cli.Commands.Abstractions/Handlers/ICliCommandHandler.cs
+++ Cli.Commands.Abstractions/Handlers/ICliCommandHandler.cs
@@ -4,6 +4,7 @@ using MediatR;
 namespace Cli.Commands.Abstractions.Handlers;
 
 // TODO: Make MediatR dependency not inheritable to other packages.
-public interface ICliCommandHandler<in TCommand> : IRequestHandler<TCommand, CliCommandOutcome[]> where TCommand : CliCommand
+public interface ICliCommandHandler<in TCommand> : IRequestHandler<TCommand, CliCommandOutcome[]>
+    where TCommand : CliCommand
 {
 }
